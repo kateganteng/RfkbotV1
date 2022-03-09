33e68f6baf014b26708ada4c454f0597b9ca3337
kateganteng <101029958+kateganteng@users.noreply.github.com>
2022-03-09 19:08:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['085725924473'] // Put your number here //owner eval
-global.kontak = ['085725924473','0'] //Ketika ada yang ngetik #owner
+global.owner = ['085954944195'] // Put your number here //owner eval
+global.kontak = ['088233832771','0'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
 global.prems = ['085725924473'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -47,8 +47,8 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
-namakontak1 = 'KAKA BOT'
-namakontak2 = 'KAKA BOT'
+namakontak1 = 'Kate Ganteng'
+namakontak2 = 'Mursid S'
 
 //kasihcaption = `Nih Kak`
 
